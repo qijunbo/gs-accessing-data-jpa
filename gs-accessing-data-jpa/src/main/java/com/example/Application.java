213446c89e0284046938c07cc129d615e4422098
DESKTOP-QECEDFM\qijunbo <junboqi@foxmail.com>
2017-09-04 20:48:26 +0800

    change the  parent type of  Application class.   make the app can run in  war  or  jar model.

--- gs-accessing-data-jpa/src/main/java/com/example/Application.java
+++ gs-accessing-data-jpa/src/main/java/com/example/Application.java
@@ -1,6 +1,5 @@
 package com.example;
 
-
 import java.util.Date;
 
 import org.slf4j.Logger;
@@ -9,28 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import com.example.jpa.repository.Customer;
 import com.example.jpa.repository.CustomerRepository;
 
 @SpringBootApplication
-public class Application  implements CommandLineRunner{
+public class Application extends SpringBootServletInitializer implements CommandLineRunner {
 
 	@Autowired
 	private CustomerRepository repository;
-	
-	
+
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
-	
+
 	@Override
 	public void run(String... strings) throws Exception {
-		
+
 		// save a couple of customers
-		repository.save(new Customer("Jack", "Bauer", "Jack@builder.com", new Date() ));
+		repository.save(new Customer("Jack", "Bauer", "Jack@builder.com", new Date()));
 		repository.save(new Customer("Chloe", "O'Brian", "Chloe@builder.com", new Date()));
 		repository.save(new Customer("Kim", "Bauer", "Kim@builder.com", new Date()));
 		repository.save(new Customer("David", "Palmer", "David@builder.com", new Date()));
@@ -43,9 +48,7 @@ public class Application  implements CommandLineRunner{
 		for (Customer customer : repository.findAll()) {
 			log.info(customer.toString());
 		}
-		 
-	 
+
 	}
- 
 
 }
